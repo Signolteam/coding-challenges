4bef781a0cd9420a66674c84bca35a20a1067d72
Dodo <dodo@dodo.org>
2023-01-15 17:29:04 +0000

    Add models and CRUD endpoints for Person, Company, Task and TaskStatus

--- backend/src/api_v1.py
+++ backend/src/api_v1.py
@@ -1,6 +1,15 @@
-from fastapi import FastAPI\
+from typing import List
 
-from src.models import create_db_schema
+from fastapi import FastAPI, HTTPException, Query
+from sqlmodel import select
+
+from src.database import create_session
+from src.models import (
+    Person, PersonRead, PersonCreate, PersonUpdate,
+    Company, CompanyRead, CompanyCreate, CompanyUpdate,
+    TaskStatus, TaskStatusRead, TaskStatusCreate, TaskStatusUpdate,
+    Task, TaskRead, TaskCreate, TaskUpdate,
+)
 
 v1 = FastAPI(
     title="Signol Tech Test - Doron",
@@ -9,7 +18,221 @@ v1 = FastAPI(
 )
 
 
-@v1.get("/hero")
-def read_hero():
-    create_db_schema()
-    return {"message": "created all models (hero)"}
+########################################################################################################################
+# Company - CRUD
+########################################################################################################################
+@v1.get("/companies/", response_model=List[CompanyRead])
+def read_companies(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with create_session() as session:
+        companies = session.exec(select(Company).offset(offset).limit(limit)).all()
+        return companies
+
+
+@v1.post("/companies/", response_model=CompanyRead)
+def create_company(company: CompanyCreate):
+    with create_session() as session:
+        db_company = Company.from_orm(company)
+        session.add(db_company)
+        session.commit()
+        session.refresh(db_company)
+        return db_company
+
+
+@v1.get("/companies/{company_id}", response_model=CompanyRead)
+def read_company(company_id: int):
+    with create_session() as session:
+        company = session.get(Company, company_id)
+        if not company:
+            raise HTTPException(status_code=404, detail="Company not found")
+        return company
+
+
+@v1.patch("/companies/{company_id}", response_model=CompanyRead)
+def update_company(company_id: int, company: CompanyUpdate):
+    with create_session() as session:
+        db_company = session.get(Company, company_id)
+        if not db_company:
+            raise HTTPException(status_code=404, detail="Company not found")
+        company_data = company.dict(exclude_unset=True)
+        for key, value in company_data.items():
+            setattr(db_company, key, value)
+        session.add(db_company)
+        session.commit()
+        session.refresh(db_company)
+        return db_company
+
+
+@v1.delete("/companies/{company_id}")
+def delete_company(company_id: int):
+    with create_session() as session:
+        company = session.get(Company, company_id)
+        if not company:
+            raise HTTPException(status_code=404, detail="Company not found")
+        session.delete(company)
+        session.commit()
+        return {"ok": True}
+
+
+########################################################################################################################
+# Person - CRUD
+########################################################################################################################
+@v1.get("/people/", response_model=List[PersonRead])
+def read_people(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with create_session() as session:
+        people = session.exec(select(Person).offset(offset).limit(limit)).all()
+        return people
+
+
+@v1.post("/people/", response_model=PersonRead)
+def create_person(person: PersonCreate):
+    with create_session() as session:
+        db_person = Person.from_orm(person)
+        session.add(db_person)
+        session.commit()
+        session.refresh(db_person)
+        return db_person
+
+
+@v1.get("/people/{person_id}", response_model=PersonRead)
+def read_person(person_id: int):
+    with create_session() as session:
+        person = session.get(Person, person_id)
+        if not person:
+            raise HTTPException(status_code=404, detail="Person not found")
+        return person
+
+
+@v1.patch("/people/{person_id}", response_model=PersonRead)
+def update_person(person_id: int, person: PersonUpdate):
+    with create_session() as session:
+        db_person = session.get(Person, person_id)
+        if not db_person:
+            raise HTTPException(status_code=404, detail="Person not found")
+        person_data = person.dict(exclude_unset=True)
+        for key, value in person_data.items():
+            setattr(db_person, key, value)
+        session.add(db_person)
+        session.commit()
+        session.refresh(db_person)
+        return db_person
+
+
+@v1.delete("/people/{person_id}")
+def delete_person(person_id: int):
+    with create_session() as session:
+        person = session.get(Person, person_id)
+        if not person:
+            raise HTTPException(status_code=404, detail="Person not found")
+        session.delete(person)
+        session.commit()
+        return {"ok": True}
+
+
+########################################################################################################################
+# TaskStatus - CRUD
+########################################################################################################################
+@v1.get("/task_status/", response_model=List[TaskStatusRead])
+def read_task_status(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with create_session() as session:
+        task_status = session.exec(select(TaskStatus).offset(offset).limit(limit)).all()
+        return task_status
+
+
+@v1.post("/task_status/", response_model=TaskStatusRead)
+def create_task_status(task_status: TaskStatusCreate):
+    with create_session() as session:
+        db_task_status = TaskStatus.from_orm(task_status)
+        session.add(db_task_status)
+        session.commit()
+        session.refresh(db_task_status)
+        return db_task_status
+
+
+@v1.get("/task_status/{task_status_id}", response_model=TaskStatusRead)
+def read_task_status(task_status_id: int):
+    with create_session() as session:
+        task_status = session.get(TaskStatus, task_status_id)
+        if not task_status:
+            raise HTTPException(status_code=404, detail="TaskStatus not found")
+        return task_status
+
+
+@v1.patch("/task_status/{task_status_id}", response_model=TaskStatusRead)
+def update_task_status(task_status_id: int, task_status: TaskStatusUpdate):
+    with create_session() as session:
+        db_task_status = session.get(TaskStatus, task_status_id)
+        if not db_task_status:
+            raise HTTPException(status_code=404, detail="TaskStatus not found")
+        task_status_data = task_status.dict(exclude_unset=True)
+        for key, value in task_status_data.items():
+            setattr(db_task_status, key, value)
+        session.add(db_task_status)
+        session.commit()
+        session.refresh(db_task_status)
+        return db_task_status
+
+
+@v1.delete("/task_status/{task_status_id}")
+def delete_task_status(task_status_id: int):
+    with create_session() as session:
+        task_status = session.get(TaskStatus, task_status_id)
+        if not task_status:
+            raise HTTPException(status_code=404, detail="TaskStatus not found")
+        session.delete(task_status)
+        session.commit()
+        return {"ok": True}
+
+
+########################################################################################################################
+# Task - CRUD
+########################################################################################################################
+@v1.get("/tasks/", response_model=List[TaskRead])
+def read_tasks(offset: int = 0, limit: int = Query(default=100, le=100)):
+    with create_session() as session:
+        tasks = session.exec(select(Task).offset(offset).limit(limit)).all()
+        return tasks
+
+
+@v1.post("/tasks/", response_model=TaskRead)
+def create_task(task: TaskCreate):
+    with create_session() as session:
+        db_task = Task.from_orm(task)
+        session.add(db_task)
+        session.commit()
+        session.refresh(db_task)
+        return db_task
+
+
+@v1.get("/tasks/{task_id}", response_model=TaskRead)
+def read_task(task_id: int):
+    with create_session() as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        return task
+
+
+@v1.patch("/tasks/{task_id}", response_model=TaskRead)
+def update_task(task_id: int, task: TaskUpdate):
+    with create_session() as session:
+        db_task = session.get(Task, task_id)
+        if not db_task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        task_data = task.dict(exclude_unset=True)
+        for key, value in task_data.items():
+            setattr(db_task, key, value)
+        session.add(db_task)
+        session.commit()
+        session.refresh(db_task)
+        return db_task
+
+
+@v1.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    with create_session() as session:
+        task = session.get(Task, task_id)
+        if not task:
+            raise HTTPException(status_code=404, detail="Task not found")
+        session.delete(task)
+        session.commit()
+        return {"ok": True}
--- backend/src/models.py
+++ backend/src/models.py
@@ -1,18 +1,125 @@
-from typing import Optional
+from datetime import date
+from typing import Optional, List
 
-from sqlmodel import Field, SQLModel, create_engine
+from sqlmodel import Field, SQLModel, Relationship
 
-from src.database import get_database_url
-
-engine = create_engine(get_database_url(), echo=True)
+from src.database import engine
 
 
 def create_db_schema():
     SQLModel.metadata.create_all(engine)
 
 
-class Hero(SQLModel, table=True):
+class ModelTableMixin(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class ModelReadMixin(SQLModel):
+    id: int
+
+
+########################################################################################################################
+# Company
+########################################################################################################################
+class CompanyBase(SQLModel):
+    name: str = Field(index=True, unique=True)
+    tasks: List["Task"] = Relationship(back_populates="company")
+
+
+class Company(CompanyBase, ModelTableMixin, table=True):
+    pass
+
+
+class CompanyCreate(CompanyBase):
+    pass
+
+
+class CompanyRead(CompanyBase, ModelReadMixin):
+    pass
+
+
+class CompanyUpdate(SQLModel):
+    name: Optional[str] = None
+
+
+########################################################################################################################
+# Person
+########################################################################################################################
+class PersonBase(SQLModel):
     name: str
-    secret_name: str
-    age: Optional[int] = None
+    email: str = Field(index=True, unique=True)
+    tasks: List["Task"] = Relationship(back_populates="created_by")
+
+
+class Person(PersonBase, ModelTableMixin, table=True):
+    pass
+
+
+class PersonCreate(PersonBase):
+    pass
+
+
+class PersonRead(ModelReadMixin, PersonBase):
+    pass
+
+
+class PersonUpdate(SQLModel):
+    name: Optional[str] = None
+    email: Optional[str] = None
+
+
+########################################################################################################################
+# TaskStatus
+########################################################################################################################
+class TaskStatusBase(SQLModel):
+    __tablename__ = "task_status"
+    name: str = Field(index=True, unique=True)
+
+
+class TaskStatus(TaskStatusBase, ModelTableMixin, table=True):
+    pass
+
+
+class TaskStatusCreate(TaskStatusBase):
+    pass
+
+
+class TaskStatusRead(ModelReadMixin, TaskStatusBase):
+    pass
+
+
+class TaskStatusUpdate(SQLModel):
+    name: Optional[str] = None
+
+
+########################################################################################################################
+# Task
+########################################################################################################################
+class TaskBase(SQLModel):
+    owner_id: int = Field(foreign_key="person.id")
+    owner: Person = Relationship(back_populates="tasks")
+    task_date: date
+    description: str = Field(max_length=1000)
+    company_id: int = Field(foreign_key="company.id")
+    company: Company = Relationship(back_populates="tasks")
+    status_id: int = Field(foreign_key="task_status.id")
+    status: TaskStatus = Relationship(back_populates="tasks")
+
+
+class Task(TaskBase, ModelTableMixin, table=True):
+    pass
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskRead(ModelReadMixin, TaskBase):
+    pass
+
+
+class TaskUpdate(SQLModel):
+    task_date: Optional[date] = None
+    description: Optional[str] = None
+    company_id: Optional[int] = None
+    status_id: Optional[int] = None
