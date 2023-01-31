d12bd998ae7587d05f5c9c2b89d09cceb9160290
Themis Theodoratos <themistheodoratos@gmail.com>
2023-01-31 16:52:43 +0000

    Task model added

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = (
+        'created_by',
+        'completed_by',
+        'company',
+        'status',
+    )
+
+admin.site.register(Task, TaskAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Approved','Approved'),
+        ('Rejected','Rejected'),
+        ('Review','Review'),
+    ]
+    description = models.CharField(max_length=1000)
+    created_by = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    completed_by = models.DateTimeField(blank=True)
+    company = models.CharField(max_length=255)
+    status = models.CharField(max_length=255, default='Review', choices=STATUS_CHOICES)
+    
+    def __str__(self):
+        return self.description
